c365ba10d026d6d86f75b46c4dc2521896e21330
Cunningham Code Consultancy <104110053+cunninghamcodeconsultancy@users.noreply.github.com>
2023-05-13 12:15:07 +0100

    Add final styles in style.css and update countdown func in app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const timeLeft = document.getElementById('time-left');
+const birthday = new Date('02/28/2024');
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24; //milliseconds
+let timerId;
+
+
+function countDown() {
+    const today = new Date();
+    const timeSpan = birthday - today;
+
+    if (timeSpan <= -day){
+        timeLeft.innerHTML = "Hope you had a nice Birthday!";
+        clearInterval(timerId);
+        return;
+    }
+    if (timeSpan <= 0) {
+        timeLeft.innerHTML = "Happy Birthday!!!";
+        clearInterval(timerId);
+        return;
+    }
+
+    const days = Math.floor(timeSpan / day);
+    const hours = Math.floor((timeSpan % day) / hour);
+    const minutes = Math.floor((timeSpan % hour) / minute);
+    const seconds = Math.floor((timeSpan % minute) / second);
+
+    timeLeft.innerHTML = days + 'days ' + hours + 'hours ' + minutes + ' min ' + seconds + 'seconds';
+}
+
+timerId = setInterval(countDown, second);
